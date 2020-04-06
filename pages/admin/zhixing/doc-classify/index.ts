161809d4d2bb00a29fd27cd1c6c6c84e46e02056
zzp-dog <1029512956@qq.com>
2020-04-06 22:28:16 +0800

    （+）添加前台知行模块和后台知行管理模块

---
+++ pages/admin/zhixing/doc-classify/index.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class DocClassifyComponent extends Vue {
+    constructor() {
+        super();
+    }
+
+}
---
+++ pages/admin/zhixing/doc-content/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    
+    </div>
+</template>
+<script lang="ts" src="./index.ts"></script>
+<style lang="scss" scoped src="./index.scss"></style>
